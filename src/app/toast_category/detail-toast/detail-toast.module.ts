cdf95f8bc921eb2c5702be1470ad1de1f00d3396
bacho <b@mail.com>
2023-04-05 00:22:49 +0400

    add, comments, like and viewvs... whithout methods

--- src/app/toast_category/detail-toast/detail-toast.module.ts
+++ src/app/toast_category/detail-toast/detail-toast.module.ts
@@ -4,13 +4,18 @@ import { CommonModule } from '@angular/common';
 import { DetailToastRoutingModule } from './detail-toast-routing.module';
 import { DetailComponentComponent } from './components/detail-component/detail-component.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 // import { ToastListRoutingModule } from './toast-list-routing.module';
 // import { CategoryComponentComponent } from './components/category-component/category-component.component';
 // import { NgxPaginationModule } from 'ngx-pagination';
 // import { HttpClient, HttpClientModule } from '@angular/common/http';
 import {MatChipsModule} from '@angular/material/chips';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,7 +25,14 @@ import {MatChipsModule} from '@angular/material/chips';
     CommonModule,
     DetailToastRoutingModule,
     ReactiveFormsModule,
-    MatChipsModule
+    MatChipsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    // BrowserAnimationsModule,
+
   ]
 })
 export class DetailToastModule { }
